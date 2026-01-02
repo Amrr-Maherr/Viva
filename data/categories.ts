b0ba6b50ac4329f76d477aa90a97ea7e3e1f1b39
Amrr-Maherr <amrr.maherr242gmail.com>
2026-01-02 21:25:26 +0200

    feat: add categories data array

---
+++ data/categories.ts
@@ -0,0 +1,12 @@
+export const categories = [
+    "Music",
+    "Men's Fashion",
+    "Women's Fashion",
+    "SuperMarket",
+    "Baby & Toys",
+    "Home",
+    "Books",
+    "Beauty & Health",
+    "Mobiles",
+    "Electronics"
+];
