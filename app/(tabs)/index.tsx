f8e321ec8608414ea6ae534a2482dc67780f4d9e
Amrr-Maherr <amrr.maherr242gmail.com>
2026-01-02 21:27:41 +0200

    feat: update home screen to display search input, category buttons, and products list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,15 +4,18 @@ import { Text, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import useFetchProducts from "@/hooks/useFetchProducts";
 import SearchInput from "@/components/SearchInput";
+import CategoryButtons from "@/components/CategoryButtons";
+import ProductsList from "@/components/ProductsList";
 
 export default function HomeScreen() {
-  // const { error, data, isLoading, isError, refetch } = useFetchProducts();
-  // console.log(data);
+  const { data } = useFetchProducts();
 
   return (
     <>
       <View style={{flex:1,paddingHorizontal:20,backgroundColor:"#fff"}}>
         <SearchInput />
+        <CategoryButtons/>
+        <ProductsList products={data?.data} />
       </View>
     </>
   );
