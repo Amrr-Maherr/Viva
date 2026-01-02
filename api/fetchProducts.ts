42245b364078554159126995648c5ef7914a974e
Amrr-Maherr <amrr.maherr242gmail.com>
2026-01-02 21:45:12 +0200

    refactor: update fetchProducts to accept category parameter

--- api/fetchProducts.ts
+++ api/fetchProducts.ts
@@ -1,13 +1,18 @@
 import { ProductsResponse } from "@/types/Products";
 import axios from "axios";
 
-const fetchProducts = async (): Promise<ProductsResponse> => {
+const fetchProducts = async (category?: string): Promise<ProductsResponse> => {
     try {
-        const response = await axios.get("https://ecommerce.routemisr.com/api/v1/products")
-        return response?.data
+        let url = 'https://ecommerce.routemisr.com/api/v1/products';
+        if (category && category !== 'all') {
+            url += `?category[in]=${category}`;
+        }
+        const response = await axios.get(url);
+        return response?.data;
     } catch (error) {
         console.log(error);
+        throw error;
     }
-}
+};
 
 export default fetchProducts
