abacfb1ccc4bf01172a3b20b9fe1075b2a1b6f23
Amrr-Maherr <amrr.maherr242gmail.com>
2026-01-02 21:44:50 +0200

    refactor: update categories data to include ids

--- data/categories.ts
+++ data/categories.ts
@@ -1,12 +1,12 @@
 export const categories = [
-    "Music",
-    "Men's Fashion",
-    "Women's Fashion",
-    "SuperMarket",
-    "Baby & Toys",
-    "Home",
-    "Books",
-    "Beauty & Health",
-    "Mobiles",
-    "Electronics"
+    { id: "6439d61c0049ad0b52b90051", name: "Music" },
+    { id: "6439d5b90049ad0b52b90048", name: "Men's Fashion" },
+    { id: "6439d58a0049ad0b52b9003f", name: "Women's Fashion" },
+    { id: "6439d41c67d9aa4ca97064d5", name: "SuperMarket" },
+    { id: "6439d40367d9aa4ca97064cc", name: "Baby & Toys" },
+    { id: "6439d3e067d9aa4ca97064c3", name: "Home" },
+    { id: "6439d3c867d9aa4ca97064ba", name: "Books" },
+    { id: "6439d30b67d9aa4ca97064b1", name: "Beauty & Health" },
+    { id: "6439d2f467d9aa4ca97064a8", name: "Mobiles" },
+    { id: "6439d2d167d9aa4ca970649f", name: "Electronics" }
 ];
